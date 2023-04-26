fa3cda845c6d9f011717394af0cad0f91a56fbe1
Edwards Fonseca <eduards_fonseca@hotmail.com>
2023-04-26 14:24:40 -0400

    borre boton de player 2, random position , agregue funcion CPU act- V.2

--- src/componentes/Tablero.jsx
+++ src/componentes/Tablero.jsx
@@ -4,35 +4,58 @@ import Casilla from './Casilla.jsx'
 import '../style/tablero.css'
 
 function Tablero() {
-//-//-// Iniciamos con la funcion declarando su estado
-const [gameBoard, setGameBoard] = useState(
-    [[1,1,1,1,1,0,0,0,0,1],
-    [0,0,0,0,0,0,0,0,0,1],
-    [0,0,0,0,0,0,0,0,0,1],
-    [0,0,0,0,0,0,0,0,0,1],
-    [0,0,0,0,0,0,0,0,0,0],
-    [1,0,0,1,1,0,0,0,0,0],
-    [1,0,0,0,0,0,0,0,0,0],
-    [1,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [1,1,1,1,0,0,0,0,0,0]],
+  //-//-// Iniciamos con la funcion declarando su estado
+  const [gameBoard, setGameBoard] = useState(
+    [[1, 1, 1, 1, 1, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 0, 0, 0, 0, 0, 0]],
     generateRandomBoard());
 
-// funcion fire
-function fire(row, col) {
+  // funcion fire
+
+  function fire(row, col) {
     let ships = document.querySelectorAll(".ship:not(.hit)");
     let newGameBoard = [...gameBoard];
     if (gameBoard[row][col] === 1) {
-      newGameBoard[row][col] = 2; // Marcar como parte de un barco hundido
-      // Actualizar la variable ships después de marcar una celda como parte de un barco hundido
+      newGameBoard[row][col] = 2;
       ships = document.querySelectorAll(".ship:not(.hit)");
-      // Verificar si se ha hundido un barco
-      if (ships.length -1 ===  0) {
+      if (ships.length - 1 === 0) {
         alert("¡Felicidades! ¡Has hundido todos los barcos!");
       }
     } else if (gameBoard[row][col] === 0) {
+      newGameBoard[row][col] = 3;
       alert("Tiro Errado")
-      newGameBoard[row][col] = 3; // Marcar como un tiro fallido
+      setTimeout(cpuTurn, 100); // Llamamos a la función cpuTurn después de un segundo
+    }
+    setGameBoard(newGameBoard);
+  }
+  function cpuTurn() {
+
+    let newGameBoard = [...gameBoard];
+    let ships = document.querySelectorAll(".ship:not(.hit)");
+    let randomRow, randomCol;
+    do {
+      randomRow = Math.floor(Math.random() * 10);
+      randomCol = Math.floor(Math.random() * 10);
+    } while (newGameBoard[randomRow][randomCol] === 2 || newGameBoard[randomRow][randomCol] === 3);
+
+    if (newGameBoard[randomRow][randomCol] === 1) {
+      newGameBoard[randomRow][randomCol] = 2;
+
+      ships = document.querySelectorAll(".ship:not(.hit)");
+      if (ships.length - 1 === 0) {
+        alert("¡La CPU ha hundido todos tus barcos!");
+
+      }
+    } else if (newGameBoard[randomRow][randomCol] === 0) {
+      newGameBoard[randomRow][randomCol] = 3;
     }
     setGameBoard(newGameBoard);
   }
@@ -44,7 +67,7 @@ function fire(row, col) {
       const row = [];
       for (let j = 0; j < 10; j++) {
         // Generar un número aleatorio entre 0 y 1
-        const random = Math.floor(Math.random() *2);
+        const random = Math.floor(Math.random() * 2);
         // el número es 1 y la celda no está ocupada, colocar un barco
         if (random === 0 && row.indexOf(1) === -1) {
           row.push(1); // lo devuelve a row con nuevo valor
@@ -60,7 +83,7 @@ function fire(row, col) {
   // Con esta llamamos a la funcion randon con su nuevo estado y la aplicamos a un boton
   function handleRandomClick() {
     setGameBoard(generateRandomBoard());
-  } 
+  }
   let rows = [];
   for (let i = 0; i < 10; i++) {
     let cells = [];
@@ -76,13 +99,11 @@ function fire(row, col) {
     rows.push(<div key={i} className="row">{cells}</div>);
   }
   return <><div className="board">{rows}
-  
+
+
   </div>
-  <div className='button-a'>
-  <button className="contador "style={{"--clr":"#1e9bff"}} type="button" onClick={handleRandomClick} ><span><i></i>Random Position</span></button>
-  
-  </div>
-  
+
+
   </>
 }
 
