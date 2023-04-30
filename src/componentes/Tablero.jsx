176d239bac6191365313a579b386b1e63365da28
Edwards Fonseca <eduards_fonseca@hotmail.com>
2023-04-30 14:56:27 -0400

    agregando nuevas funciones de modificar casilla

--- src/componentes/Tablero.jsx
+++ src/componentes/Tablero.jsx
@@ -18,24 +18,28 @@ function Tablero() {
     [1, 1, 1, 1, 0, 0, 0, 0, 0, 0]],
     generateRandomBoard());
   const [addShipsManually, setAddShipsManually] = useState(false)
+  const [gameStarted, setGameStarted] = useState(false);
+  const [selectedCell, setSelectedCell] = useState(null);
 
   // funcion fire
 
   function fire(row, col) {
-    let ships = document.querySelectorAll(".ship:not(.hit)");
-    let newGameBoard = [...gameBoard];
-    if (gameBoard[row][col] === 1) {
-      newGameBoard[row][col] = 2;
-      ships = document.querySelectorAll(".ship:not(.hit)");
-      if (ships.length - 1 === 0) {
-        alert("¡Felicidades! ¡Has hundido todos los barcos!");
+    if (gameStarted) {
+      let ships = document.querySelectorAll(".ship:not(.hit)");
+      let newGameBoard = [...gameBoard];
+      if (gameBoard[row][col] === 1) {
+        newGameBoard[row][col] = 2;
+        ships = document.querySelectorAll(".ship:not(.hit)");
+        if (ships.length - 1 === 0) {
+          alert("¡Felicidades! ¡Has hundido todos los barcos!");
+        }
+      } else if (gameBoard[row][col] === 0) {
+        newGameBoard[row][col] = 3;
+        alert("Tiro Errado")
+        setTimeout(cpuTurn, 100); // Llamamos a la función cpuTurn después de un segundo
       }
-    } else if (gameBoard[row][col] === 0) {
-      newGameBoard[row][col] = 3;
-      alert("Tiro Errado")
-      setTimeout(cpuTurn, 100); // Llamamos a la función cpuTurn después de un segundo
+      setGameBoard(newGameBoard);
     }
-    setGameBoard(newGameBoard);
   }
   function cpuTurn() {
 
@@ -68,7 +72,7 @@ function Tablero() {
       const row = [];
       for (let j = 0; j < 10; j++) {
         // Generar un número aleatorio entre 0 y 1
-        const random = Math.floor(Math.random() *2);
+        const random = Math.floor(Math.random() * 2);
         // el número es 1 y la celda no está ocupada, colocar un barco
         if (random === 0 && row.indexOf(1) === -1) {
           row.push(1); // lo devuelve a row con nuevo valor
@@ -80,10 +84,7 @@ function Tablero() {
     }
     return newBoard;
   }
-function startGame(){
-  setAddShipsManually(false)
 
-}
   // Con esta llamamos a la funcion randon con su nuevo estado y la aplicamos a un boton
   /* function handleRandomClick() {
     setGameBoard(generateRandomBoard());
@@ -98,13 +99,16 @@ function startGame(){
   }
 
   function handleCellClick(row, col) {
-    
-    if (addShipsManually) {
+    if (addShipsManually && !gameStarted) {
       const newGameBoard = [...gameBoard];
       newGameBoard[row][col] = 1;
       setGameBoard(newGameBoard);
-    } 
+    }
   }
+  function startGame() {
+    setGameStarted(true);
+  }
+
 
   const rows = [];
   for (let i = 0; i < 10; i++) {
@@ -114,24 +118,31 @@ function startGame(){
         <Casilla
           key={`${i},${j}`}
           status={gameBoard[i][j]}
+          onClick={() => fire(i, j)}
           handleClick={() => handleCellClick(i, j)}
+          
         />
       );
     }
     rows.push(<div key={i} className="row">{cells}</div>);
   }
-  return <><div className="board">{rows}
-  
 
-  
-
-  </div>
-  <button className="contador "style={{"--clr":"#1e9bff"}} type="button" onClick={handleManualClick} ><span><i></i>Random Position</span></button>
-  <button id="start-btn" onClick={startGame}>Iniciar Juego</button>
-  <button onClick={handleResetClick}>Reset</button>
-
-
-  </>
+  return (
+    <>
+      <div className="board">{rows}</div>
+      {!gameStarted && (
+        <div className="buttons">
+          <button onClick={handleManualClick}>Colocar barcos manualmente</button>
+          <button onClick={() => setGameBoard(generateRandomBoard())}>Colocar barcos aleatoriamente</button>
+          <button onClick={handleResetClick}>Resetear tablero</button>
+          <button onClick={startGame}>Empezar juego</button>
+        </div>
+      )}
+      {addShipsManually && !gameStarted && (
+        <p>Haz clic en las casillas para colocar tus barcos</p>
+      )}
+    </>
+  );
 }
 
 export default Tablero
